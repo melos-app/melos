22b87b684e5f87a60d191e093522f29e8763812b
Cliff Brake <cbrake@bec-systems.com>
2025-01-30 10:23:41 -0500

    add file dialog

--- cmd/melos-gui/main.go
+++ cmd/melos-gui/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
+
+	"fyne.io/fyne"
 	"fyne.io/fyne/app"
+	"fyne.io/fyne/dialog"
 	"fyne.io/fyne/widget"
 )
 
@@ -9,6 +13,22 @@ func main() {
 	a := app.New()
 	w := a.NewWindow("Melos")
 
+	fileButton := widget.NewButton("Select File", func() {
+		dialog.ShowFileOpen(func(f fyne.URIReadCloser, err error) {
+			if err != nil {
+				dialog.ShowError(err, w)
+				return
+			}
+			if f == nil {
+				return
+			}
+			selectedFile := f.URI()
+			fmt.Println("Selected file: ", selectedFile) // Do something with the selected file
+		}, w)
+	})
+
 	w.SetContent(widget.NewLabel("Hello, Melos!"))
+	w.SetContent(fileButton)
 	w.ShowAndRun()
+
 }
