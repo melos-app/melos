553a07e61c846b882d80b3c087321205ee05883c
Cliff Brake <cbrake@bec-systems.com>
2025-01-31 08:42:27 -0500

    file select and update label working

--- cmd/melos-gui/main.go
+++ cmd/melos-gui/main.go
@@ -1,19 +1,15 @@
 package main
 
 import (
-	"fmt"
-
 	"fyne.io/fyne"
 	"fyne.io/fyne/app"
+	"fyne.io/fyne/container"
 	"fyne.io/fyne/dialog"
 	"fyne.io/fyne/widget"
 )
 
-func main() {
-	a := app.New()
-	w := a.NewWindow("Melos")
-
-	fileButton := widget.NewButton("Select File", func() {
+func fileButton(w fyne.Window, callback func(string)) *widget.Button {
+	return widget.NewButton("Select Typst file", func() {
 		dialog.ShowFileOpen(func(f fyne.URIReadCloser, err error) {
 			if err != nil {
 				dialog.ShowError(err, w)
@@ -22,13 +18,28 @@ func main() {
 			if f == nil {
 				return
 			}
-			selectedFile := f.URI()
-			fmt.Println("Selected file: ", selectedFile) // Do something with the selected file
+			selectedFile := f.URI().String()
+			callback(selectedFile)
 		}, w)
 	})
+}
 
-	w.SetContent(widget.NewLabel("Hello, Melos!"))
-	w.SetContent(fileButton)
+func main() {
+	a := app.New()
+	w := a.NewWindow("Melos")
+
+	filePathDisplay := widget.NewLabel("")
+
+	setFilePath := func(p string) {
+		filePathDisplay.SetText(p)
+	}
+
+	w.SetContent(container.NewVBox(
+		widget.NewLabel("Hello, Melos!"),
+		fileButton(w, setFilePath),
+		filePathDisplay,
+	))
+
+	w.Resize(fyne.NewSize(600, 400))
 	w.ShowAndRun()
-
 }
