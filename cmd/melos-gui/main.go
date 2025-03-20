f78a1f99613e7df49ac3b557f5f3643e29ce38aa
Cliff Brake <cbrake@bec-systems.com>
2025-03-20 08:28:32 -0400

    add checkboxes

--- cmd/melos-gui/main.go
+++ cmd/melos-gui/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"fyne.io/fyne"
 	"fyne.io/fyne/app"
 	"fyne.io/fyne/container"
@@ -34,10 +36,47 @@ func main() {
 		filePathDisplay.SetText(p)
 	}
 
+	// steps:
+	//  - uncompress
+	//  - removeTitles
+	//  - generateSvg
+	//  - updateTypst
+	//  - makeBook
+
+	uncompress := false
+	removeTitles := false
+	generateSvg := false
+	updateTypst := false
+	makeBook := false
+
+	// TODO: set checkbox to checked by default
+
 	w.SetContent(container.NewVBox(
 		widget.NewLabel("Hello, Melos!"),
 		fileButton(w, setFilePath),
 		filePathDisplay,
+		widget.NewCheck("Uncompress", func(checked bool) {
+			uncompress = checked
+		}),
+		widget.NewCheck("Remove Titles", func(checked bool) {
+			removeTitles = checked
+		}),
+		widget.NewCheck("Generate SVG", func(checked bool) {
+			generateSvg = checked
+		}),
+		widget.NewCheck("Update Typst file", func(checked bool) {
+			updateTypst = checked
+		}),
+		widget.NewCheck("Make book", func(checked bool) {
+			makeBook = checked
+		}),
+		widget.NewButton("Run", func() {
+			fmt.Println("uncompress:", uncompress)
+			fmt.Println("removeTitles:", removeTitles)
+			fmt.Println("generateSvg:", generateSvg)
+			fmt.Println("updateTypst:", updateTypst)
+			fmt.Println("makeBook:", makeBook)
+		}),
 	))
 
 	w.Resize(fyne.NewSize(600, 400))
